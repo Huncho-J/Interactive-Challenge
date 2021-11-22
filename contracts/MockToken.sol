96b353f9ddfba68f86072be7be3aa7c9306be1f3
Huncho-J <77946521+Huncho-J@users.noreply.github.com>
2021-11-22 09:55:51 -0800

    Update MockToken.sol

--- contracts/MockToken.sol
+++ contracts/MockToken.sol
@@ -1,3 +1,4 @@
+//"SPDX-License-Identifier: <SPDX-License>
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
@@ -5,13 +6,12 @@ contract MockToken is ERC20{
     uint256 public startTime;
     uint256 public endTime;
 
-    constructor(uint256 _startTokenRelease, uint256 _endTokenRelease)  ERC20("TOKEN", "TKN"){
+    constructor()  ERC20("TOKEN", "TKN"){
       _mint(msg.sender,100000);
 
-      startTime = _startTokenRelease;
-      endTime = _endTokenRelease;
+
     }
-  function setStartTime(){
+  function setStartTime() public {
     //enable token transfers
   }
 
