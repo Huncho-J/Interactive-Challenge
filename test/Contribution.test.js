36c034a27f70c694448e541086615252707a8ee3
Huncho-J <77946521+Huncho-J@users.noreply.github.com>
2021-11-22 09:55:56 -0800

    Update Contribution.test.js

---
+++ test/Contribution.test.js
@@ -0,0 +1,74 @@
+//load contract artifacts
+const ContributionContract = artifacts.require("./Contribution.sol");
+const MockToken = artifacts.require("./MockToken.sol");
+
+const assert = require("chai").assert;
+const truffleAssert = require('truffle-assertions');
+
+//initialize test for contribution.sol
+contract('Contribution', function(accounts){
+
+// build up new contracts before each test
+  let contributionContract, mockToken, result
+
+  before(async () => {
+    mockToken = await MockToken.deployed()
+    contributionContract = await ContributionContract.deployed(mockToken.address)
+    result = await web3.eth.sendTransaction({
+      from: accounts[3],
+      to: contributionContract.address,
+      value:'5'
+    })
+  })
+
+//check for successful deployment
+  describe('deployment', async() => {
+//check for contribution contract address
+    it('deploys succesfully', async () => {
+      const address = await contributionContract.address
+      assert.notEqual(address, 0x0)
+      assert.notEqual(address, '')
+      assert.notEqual(address, null)
+      assert.notEqual(address, undefined)
+    })
+//check for contribution contract has access to MockToken Contract
+    it('it has MockTokenContract', async () =>{
+      mockTokenAddress = await contributionContract.tokenContract();
+      assert.equal(mockToken.address, mockTokenAddress)
+      const tokenBalance = await mockToken.balanceOf(contributionContract.address)
+      console.log(tokenBalance.toNumber())
+    })
+  })
+//Test for receieve() fallback function
+    describe('recieve()', async () =>{
+      //test for failed transaction. Not enough tokens in contract
+      it("should not be able to send more than the availble token balance", async () => {
+        await truffleAssert.reverts(
+          web3.eth.sendTransaction({
+            from: accounts[2],
+            to: contributionContract.address,
+            value:'5000000000'
+          }),
+            "Not enough tokens in contract"
+        );
+    });
+    //check for successful transfer after donation
+    it('transfers tokens to donator', async () =>{
+      const donatorBal =  await mockToken.balanceOf(accounts[3])
+      assert.equal(donatorBal.toNumber(), 10)
+    })
+    //check for totalDonation of accounts 3
+    it('updates donator list', async () =>{
+      const ethDonation = await contributionContract.DonatorList(accounts[3])
+      assert.equal(ethDonation.toNumber(), 5)
+    })
+    // it('emits recieve event', async () =>{
+    // const event = result.logs[0].args
+    // assert.equal(event.donator, accounts[3], 'Logs the correct account')
+    // assert.equal(event.totalDonation, 5, 'Logs correct donation amount')
+    // })
+  })
+
+
+
+})
