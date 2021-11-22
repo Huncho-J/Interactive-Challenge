f59f399a422091e8f8af5cfc3f3d004eaa9f8279
Huncho-J <77946521+Huncho-J@users.noreply.github.com>
2021-11-22 14:20:26 -0800

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,15 +6,16 @@ module.exports = async  function (deployer) {
   //load preset accounts from Ganache
   const accounts = await web3.eth.getAccounts();
   //deploy token contract
-  await deployer.deploy(MockToken);
+  await deployer.deploy(MockToken,1637613758, 1637614058);
   const mockTokenContract = await MockToken.deployed();
 
   //deploy ContributionContract
   await deployer.deploy(ContributionContract,mockTokenContract.address);
   const contributionContract = await ContributionContract.deployed();
 
-  await mockTokenContract.transfer(contributionContract.address, ('1000'))
-
+//transfer tokens to contribution contract
+  await mockTokenContract.transfer(contributionContract.address, 1000)
+//send some ether using account 2
   await web3.eth.sendTransaction({
     from: accounts[1],
     to: contributionContract.address,
