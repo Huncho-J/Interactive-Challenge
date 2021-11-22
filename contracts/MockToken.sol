68254fbd10b39b8183d2a5285bd3276c3bd5901f
Huncho-J <77946521+Huncho-J@users.noreply.github.com>
2021-11-22 14:20:23 -0800

    Update MockToken.sol

--- contracts/MockToken.sol
+++ contracts/MockToken.sol
@@ -1,22 +1,35 @@
-//"SPDX-License-Identifier: <SPDX-License>
+//SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MockToken is ERC20{
+
+contract MockToken is ERC20, Pausable, Ownable{
     uint256 public startTime;
     uint256 public endTime;
 
-    constructor()  ERC20("TOKEN", "TKN"){
-      _mint(msg.sender,100000);
-
-
+    constructor(uint _startTime, uint _endTime)  ERC20("TOKEN", "TKN") {
+     _mint(msg.sender,100000);
+      startTime = _startTime;
+      endTime = _endTime;
     }
-  function setStartTime() public {
-    //enable token transfers
-  }
-
-function setEndTime() public {
-
-}
+//pause contract after specified endTime
+  function pause() public onlyOwner {
+    require(block.timestamp < endTime);
+        _pause();
+    }
+//unpause contract after specified startTime
+    function unpause() public onlyOwner {
+      require(block.timestamp >= startTime);
+        _unpause();
+    }
+//override _beforeTransferFunction to only be called when not paused
+function _beforeTokenTransfer(address from, address to, uint256 amount) internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
 
 }
