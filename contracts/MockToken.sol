472f24bc0e52b62e97ba7708a242af5141976034
Huncho-J <77946521+Huncho-J@users.noreply.github.com>
2021-11-26 06:49:17 -0800

    Update MockToken.sol

--- contracts/MockToken.sol
+++ contracts/MockToken.sol
@@ -12,20 +12,27 @@ contract MockToken is ERC20, Pausable, Ownable{
     event Unpaused(bool status, uint time);
 
     constructor(uint _startTime, uint _endTime)  ERC20("TOKEN", "TKN") {
+      //mint 1million worth to deployer
      _mint(msg.sender,1000000000000000000000000);
       startTime = _startTime;
       endTime = _endTime;
     }
-//pause contract after specified endTime
-  function pause() public onlyOwner {
-    require(block.timestamp <= endTime, "Not the right end time");
-    emit Paused(true, endTime);
-    _pause();
-    }
+  //pause contract after specified endTime
+    function pause() public onlyOwner {
+      //check for endTime set
+      require(block.timestamp <= endTime, "Not the right end time");
+      //emit event
+      emit Paused(true, endTime);
+      //call pause() from openzeppelin
+      _pause();
+      }
 //unpause contract after specified startTime: use open zepplin pause modifier
     function unpause() public onlyOwner whenPaused {
+      //check for startTime set
       require(block.timestamp >= startTime, "Not the right start time");
+      //emit event
       emit Unpaused(false, startTime);
+      //call pause() from openzeppelin
       _unpause();
     }
 //override _beforeTransferFunction to only be called when not paused
