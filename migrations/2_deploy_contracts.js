32d204b8fed1d06653945fcc534100742232141d
Huncho-J <77946521+Huncho-J@users.noreply.github.com>
2021-11-25 17:58:06 -0800

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,11 +2,14 @@ const MockToken = artifacts.require("./MockToken.sol");
 const ContributionContract = artifacts.require("./Contribution.sol");
 const Web3 = require('web3');
 
+function tokens(n){
+  return web3.utils.toWei(n, 'ether');
+}
 module.exports = async  function (deployer) {
   //load preset accounts from Ganache
   const accounts = await web3.eth.getAccounts();
   //deploy token contract
-  await deployer.deploy(MockToken,"uint startTime ", "uint endTime");
+  await deployer.deploy(MockToken,1637613758, 1637614058);
   const mockTokenContract = await MockToken.deployed();
 
   //deploy ContributionContract
@@ -14,12 +17,7 @@ module.exports = async  function (deployer) {
   const contributionContract = await ContributionContract.deployed();
 
 //transfer tokens to contribution contract
-  await mockTokenContract.transfer(contributionContract.address, 1000)
+  await mockTokenContract.transfer(contributionContract.address, tokens('100000'))
 //send some ether using account 2
-  await web3.eth.sendTransaction({
-    from: accounts[1],
-    to: contributionContract.address,
-    value:'2'
-  })
 
 };
